6d3bb08ea443352518b479380738a0f98d250436
dnsalves <dns_alves@hotmail.com>
2019-07-20 15:08:53 -0300

    Drop encadeado - 20/07/19

---
+++ WebApplicationSistemaPesquisaFinal/Scripts/script.js
@@ -0,0 +1,9 @@
+﻿"use strict";
+
+function UpdateQuestion(val) {
+    location.href = `?PesquisaId=${val}`;
+}
+function UpdateAlternative(val) {
+    var pesquisa = $('#PesquisaId').val()
+    location.href = `?PesquisaId=${pesquisa}&QuestaoId=${val}`;
+}
